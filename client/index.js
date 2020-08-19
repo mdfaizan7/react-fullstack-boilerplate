bd3a76fb3f42bed3456ab0f89bae3e12f39100c3
mdfaizan7 <faizaanazim@gmail.com>
2020-08-19 14:48:24 +0530

    scaffolded webpack with react

---
+++ client/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const Index = () => {
+  return <div>Welcome to React!</div>;
+};
+
+ReactDOM.render(<Index />, document.getElementById('root'));
