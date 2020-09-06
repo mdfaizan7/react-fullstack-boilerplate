5c7d096efba635bede4d2889bf93268a058071c3
mdfaizan7 <faizaanazim@gmail.com>
2020-09-06 23:54:32 +0530

    added compression-lugin and mini-css plugin for webpack

--- webpack.config.js
+++ webpack.config.js
@@ -2,66 +2,80 @@ const path = require('path')
 const webpack = require('webpack')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
+const CompressionPlugin = require('compression-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
 module.exports = {
-	devServer: {
-		contentBase: './dist',
-	},
-	entry: './client/index.js',
-	output: {
-		path: path.join(__dirname, 'dist'),
-		filename: '[name].js',
-	},
-	plugins: [
-		new HtmlWebpackPlugin({
-			template: './client/index.html',
-			filename: './index.html',
-		}),
-		new webpack.DefinePlugin({
-			'process.env': {
-				NODE_ENV: JSON.stringify('production'),
-			},
-		}),
-		new UglifyJsPlugin({
-			uglifyOptions: {
-				mangle: true,
-				compress: {
-					pure_getters: true,
-					unsafe: true,
-					unsafe_comps: true,
-					conditionals: true,
-					unused: true,
-					comparisons: true,
-					sequences: true,
-					dead_code: true,
-					evaluate: true,
-					if_return: true,
-					join_vars: true,
-				},
-				output: {
-					comments: false,
-				},
-				exclude: [/\.min\.js$/gi],
-			},
-		}),
-	],
-	module: {
-		rules: [
-			{
-				test: /\.(js|jsx)$/,
-				exclude: /node_modules/,
-				use: {
-					loader: 'babel-loader',
-				},
-			},
-			{
-				test: /\.s?css$/,
-				use: ['style-loader', 'css-loader', 'sass-loader'],
-			},
-			{
-				test: /\.(png|jpeg|jpg|woff|woff2|eot|ttf|svg|ico|json)$/,
-				loader: 'url-loader?limit=10000000',
-			},
-		],
-	},
+  devServer: {
+    contentBase: './dist'
+  },
+  entry: './client/index.js',
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: '[name].js'
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './client/index.html',
+      filename: './index.html'
+    }),
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: JSON.stringify('production')
+      }
+    }),
+    new UglifyJsPlugin({
+      uglifyOptions: {
+        mangle: true,
+        compress: {
+          pure_getters: true,
+          unsafe: true,
+          unsafe_comps: true,
+          conditionals: true,
+          unused: true,
+          comparisons: true,
+          sequences: true,
+          dead_code: true,
+          evaluate: true,
+          if_return: true,
+          join_vars: true
+        },
+        output: {
+          comments: false
+        },
+        exclude: [/\.min\.js$/gi]
+      }
+    }),
+    new CompressionPlugin({
+      filename: '[path].gz[query]',
+      algorithm: 'gzip',
+      test: /\.js$|\.css$|\.html$|\.eot?.+$|\.ttf?.+$|\.woff?.+$|\.svg?.+$/,
+      threshold: 10240,
+      minRatio: 0.8
+    }),
+    new MiniCssExtractPlugin({
+      filename: '[name].css',
+      chunkFilename: '[id].css'
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        include: path.resolve(__dirname, 'client'),
+        use: {
+          loader: 'babel-loader'
+        }
+      },
+      {
+        test: /\.s?css$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
+      },
+      {
+        test: /\.(png|jpeg|jpg|woff|woff2|eot|ttf|svg|ico|json)$/,
+        loader: 'url-loader?limit=10000000'
+      }
+    ]
+  }
 }
