6a84bba54479f6376cc313bf23e2bb587cb7042c
mdfaizan7 <faizaanazim@gmail.com>
2020-09-07 00:52:11 +0530

    updated the structure

--- server/index.js
+++ server/index.js
@@ -1,25 +1,3 @@
-import express from 'express'
-import * as path from 'path'
-
-const app = express()
-
-const port = process.env.PORT || 3000
-
-app.get('/api/greet', (req, res) => {
-  res.status(200).json({ greet: 'React' })
-})
-
-app.listen(port, () => {
-  console.log('App is running on port:', port)
-})
-
-if (process.env.NODE_ENV === 'production') {
-  const DIST_DIR = path.join(__dirname, '../dist')
-  const HTML_FILE = path.join(DIST_DIR, 'index.html')
-
-  app.use(express.static(DIST_DIR))
-
-  app.get('/*', (req, res) => {
-    res.sendFile(HTML_FILE)
-  })
-}
+// eslint-disable-next-line no-global-assign
+require = require('esm')(module)
+module.exports = require('./server.js')
