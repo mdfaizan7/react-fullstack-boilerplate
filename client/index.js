9f672b8506c3e5a6dda0c025327fec1fbfc8e645
mdfaizan7 <faizaanazim@gmail.com>
2020-09-07 14:32:06 +0530

    registered the service worker

--- client/index.js
+++ client/index.js
@@ -5,3 +5,16 @@ import App from './App'
 import './index.scss'
 
 ReactDOM.render(<App />, document.getElementById('root'))
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker
+      .register('/service-worker.js')
+      .then(registration => {
+        console.log('SW registered: ', registration)
+      })
+      .catch(registrationError => {
+        console.log('SW registration failed: ', registrationError)
+      })
+  })
+}
