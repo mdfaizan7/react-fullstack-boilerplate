6a202292023ae065a534eda066f2992485deff12
mdfaizan7 <faizaanazim@gmail.com>
2020-09-13 15:18:45 +0530

    ♻️ refactored code

--- client/App.tsx
+++ client/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
-import './App.scss'
+import './styles/App.scss'
 
 import home from './pages/home'
 import page2 from './pages/page2'
